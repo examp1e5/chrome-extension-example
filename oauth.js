0c14e2be701c780f39a1faf77d2ad18c92cbfa89
Devin Chen <d3vin.chen@gmail.com>
2020-09-10 03:17:32 +0800

    google oauth2

---
+++ oauth.js
@@ -0,0 +1,38 @@
+window.onload = function () {
+  document.querySelector("button").addEventListener("click", function () {
+    chrome.identity.getAuthToken({ interactive: true }, function (token) {
+      let init = {
+        method: "GET",
+        async: true,
+        headers: {
+          Authorization: "Bearer " + token,
+          "Content-Type": "application/json",
+        },
+        contentType: "json",
+      };
+      fetch(
+        "https://people.googleapis.com/v1/contactGroups/all?maxMembers=20",
+        init
+      )
+        .then((response) => response.json())
+        .then(function (data) {
+          let photoDiv = document.querySelector("#friendDiv");
+          let returnedContacts = data.memberResourceNames;
+          for (let i = 0; i < returnedContacts.length; i++) {
+            fetch(
+              "https://people.googleapis.com/v1/" +
+                returnedContacts[i] +
+                "?personFields=photos",
+              init
+            )
+              .then((response) => response.json())
+              .then(function (data) {
+                let profileImg = document.createElement("img");
+                profileImg.src = data.photos[0].url;
+                photoDiv.appendChild(profileImg);
+              });
+          }
+        });
+    });
+  });
+};
